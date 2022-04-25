9a5ed6e6b46d86a1cf423c988d77754793946cc9
ErickCasGlez <erick.cas.glez@gmail.com>
2022-04-25 00:00:48 -0500

    Definiciones
    Definiciones de getters, setters, clases, atributos privados

---
+++ 1/1.py
@@ -0,0 +1,34 @@
+from importlib.util import set_loader
+
+
+class Proyecto:
+    def __init__(self, codigo, nombre, precio): #self se refiere a los mismos atributos de la clase
+        self.__codigo = codigo #__ aquí se refiere a un atributo privado
+        self.__nombre = nombre
+        self.__precio = precio
+
+#Aquí se definen los getters y setters
+
+    @property #getter
+    def codigo(self):
+        return self.__codigo
+
+    @codigo.setter #setter
+    def codigo(self, valor):
+        self.__codigo = valor
+        
+    @property #getter
+    def nombre(self):
+        return self.__nombre
+
+    @codigo.setter #setter
+    def nombre(self, valor):
+        self.__nombre = valor
+
+    @property #getter
+    def precio(self):
+        return self.__precio
+
+    @codigo.setter #setter
+    def precio(self, valor):
+        self.__precio = valor
